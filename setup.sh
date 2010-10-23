be70ab5635207bcb6b3a0fca492ddf72e9d24041
natto <natto@natto-mbp.local>
2010-10-23 14:14:51 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
-#ln -s ~/dot/.vimrc ~/.vimrc
+#ln -s ~/dot/.emacs ~/.emacs
 
+echo "setting up vim..."
+#ln -s ~/dot/.vimrc ~/.vimrc
 echo "setting up vim-pathogen..."
 ln -s ./.vim/vim-pathogen/autoload/pathogen.vim ./.vim/autoload/pathogen.vim
-
-#ln -s ~/dot/.emacs ~/.emacs
 
 cd .vim/autoload
 ln -s /Users/natto/.vim/vim-pathogen/autoload/pathogen.vim pathogen.vim
 cd -
+
+# submodule command sample
+# git submodule add http://github.com/scrooloose/nerdtree.git .vim/bundle/nerdtree
