7668099156fac944ef96429151b4c3ba0f777a7d
natto <natto@natto-tp.(none)>
2011-11-27 23:01:54 -0500

    greedyview to view, support traditional L_Alt-tab, cyclews

--- xmonad.hs
+++ xmonad.hs
@@ -1,13 +1,50 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig
+import System.IO
+
 import XMonad.Util.EZConfig
 import XMonad.Config.Gnome
+import XMonad.Actions.SimpleDate
+import XMonad.Layout.ThreeColumns
+
+import qualified XMonad.StackSet as W
+import XMonad.Actions.CycleWS
+import XMonad.Actions.CycleWindows
 
 -- check xmodmap -pm to see mod key mapping
 
 main = do
     xmonad $ gnomeConfig
-      { terminal = "gnome-terminal" 
-      , borderWidth = 1
-      , modMask = mod3Mask
-      , focusedBorderColor = "red"
-      }
+        { terminal = "gnome-terminal" 
+        , borderWidth = 1
+        , modMask = myModMask
+        -- , focusedBorderColor = "red"
+        -- , manageHook = manageDocks -- <+> manageHook gnomeConfig
+        -- , layoutHook = avoidStruts $ layoutHook gnomeConfig
+        , workspaces = myWorkspaces
+        } `additionalKeys` myKeys
+
+myModMask = mod3Mask
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+
+myKeys = [] ++
+         -- cycle windows, emulate old-style alt-tab
+         [ ((mod1Mask,               xK_Tab   ), windows W.focusDown)
+         ] ++
+         -- a basic CycleWS setup
+         [ ((myModMask,               xK_Down),  nextWS)
+         , ((myModMask,               xK_Up),    prevWS)
+         , ((myModMask .|. shiftMask, xK_Down),  shiftToNext)
+         , ((myModMask .|. shiftMask, xK_Up),    shiftToPrev)
+         , ((myModMask,               xK_Right), nextScreen)
+         , ((myModMask,               xK_Left),  prevScreen)
+         , ((myModMask .|. shiftMask, xK_Right), shiftNextScreen)
+         , ((myModMask .|. shiftMask, xK_Left),  shiftPrevScreen)
+         , ((myModMask,               xK_z),     toggleWS)
+         ] ++
+         [((m .|. mod3Mask, k), windows $ f i)
+              | (i, k) <- zip myWorkspaces [xK_1 .. xK_9]
+              , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
