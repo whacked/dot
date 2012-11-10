ec773ce0bf6c12c1c8661d1c9f81802891a737a2
natto <natto@natto-tp.(none)>
2012-11-10 11:08:42 -0500

    zsh section with some outdated stuff?

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-for DOTFILE in .emacs .vim .tmux.conf .bashrc .Rprofile; do
+for DOTFILE in .emacs .vim .tmux.conf .bashrc .Rprofile .zshrc; do
     if [ -e ~/$DOTFILE ] && [ ! -h ~/$DOTFILE ]; then
         BAK=~/`date +%F`$DOTFILE
         echo -e "moving: $DOTFILE\\t->\\t$BAK"
@@ -33,10 +33,23 @@ echo "setting up tmux..."
 if [ ! -e ~/.tmux.conf ]; then
     ln -s ~/dot/.tmux.conf ~/.tmux.conf
 fi
-# TMUX ----------------------------------------------------------------
+
+# R -------------------------------------------------------------------
 echo "setting up R..."
 if [ ! -e ~/.Rprofile ]; then
     ln -s ~/dot/.Rprofile ~/.Rprofile
+fi
+
+# ZSH -----------------------------------------------------------------
+echo "setting up ZSH..."
+if [ ! -e ~/.zshrc ]; then
+    ln -s ~/dot/.zshrc ~/.zshrc
+    mkdir -p ~/.zsh/func
+    for f in ~/opt/zsh-git/functions/*; do
+        ln -s $f ~/.zsh/func/`basename $f`
+    done
+    echo 'fpath=($fpath $HOME/.zsh/func)' >> ~/.zshenv
+    echo 'typeset -U fpath' >> ~/.zshenv
 fi
 
 # LEIN ----------------------------------------------------------------
@@ -54,28 +67,21 @@ else
     echo leiningen not installed
 fi
 
-## submodule command sample
-## git submodule add http://github.com/scrooloose/nerdtree.git .vim/bundle/nerdtree
-#
-#echo "syncing git submodules..."
-#git submodule init
-#git submodule update
-#
-#echo "hacky gitignore for vundle, because it generates tags..."
-#IGNOREFILE=~/dot/.vim/bundle/vundle/.gitignore
-#if [ -e $IGNOREFILE ]; then
-#    echo "ignore file exists already"
-#else
-#    echo "generating..."
-#    echo '.netrwhist' >> $IGNOREFILE
-#    echo '.gitignore' >> $IGNOREFILE
-#    echo 'doc/tags' >> $IGNOREFILE
-#fi
-#
-#ln -s ~/dot/.zshrc ~/.zshrc
-#mkdir -p ~/.zsh/func
-#for f in ~/opt/zsh-git/functions/*; do
-#    ln -s $f ~/.zsh/func/`basename $f`
-#done
-#echo 'fpath=($fpath $HOME/.zsh/func)' >> ~/.zshenv
-#echo 'typeset -U fpath' >> ~/.zshenv
+# submodule command sample
+# git submodule add http://github.com/scrooloose/nerdtree.git .vim/bundle/nerdtree
+
+echo "syncing git submodules..."
+git submodule init
+git submodule update
+
+echo "hacky gitignore for vundle, because it generates tags..."
+IGNOREFILE=~/dot/.vim/bundle/vundle/.gitignore
+if [ -e $IGNOREFILE ]; then
+    echo "ignore file exists already"
+else
+    echo "generating..."
+    echo '.netrwhist' >> $IGNOREFILE
+    echo '.gitignore' >> $IGNOREFILE
+    echo 'doc/tags' >> $IGNOREFILE
+fi
+
