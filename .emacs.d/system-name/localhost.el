f6929044e7208870baf559906a09017aef696855
natto <natto@natto-tp.(none)>
2012-11-10 20:06:46 -0500

    remove custom sources from non-tp loading

--- .emacs.d/system-name/localhost.el
+++ .emacs.d/system-name/localhost.el
@@ -18,19 +18,17 @@
 (setq x-select-enable-clipboard t)
 (setq visual-line-mode t)
 
-(set-frame-size (selected-frame) 207 21)
-(set-frame-position (selected-frame) 0 0)
+(when (display-graphic-p)
+  (set-frame-size (selected-frame) 207 21)
+  (set-frame-position (selected-frame) 0 0))
+
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (load-file "~/.emacs.d/custom/package-management.el")
 (setq my-packages
-      (append
-       '(el-get package
-         ;; put el-get bundled packages here
-         magit yaml-mode
-         windata tree-mode ;; required for dirtree
-         )
-       (mapcar 'el-get-source-name el-get-sources)))
+      '(el-get package
+               ;; put el-get bundled packages here
+               magit yaml-mode))
 (el-get 'sync my-packages)
 
 
