273e058b44a07c81112c79ec1f91148f4cdb7182
natto <natto@natto-tp.(none)>
2012-11-23 12:11:52 -0500

    kill popup frame when dismissed (emacs 24 new default behavior?)

--- .emacs.d/system-name/natto-tp.el
+++ .emacs.d/system-name/natto-tp.el
@@ -126,6 +126,13 @@
       '((top . 0) (left . 0)
         (width . 84) (height . 60)))
 
+;; new behavior in emacs 24?
+;; http://superuser.com/questions/397806/emacs-modify-quit-window-to-delete-buffer-not-just-bury-it
+(defadvice quit-window (before quit-window-always-kill)
+  "When running `quit-window', always kill the buffer."
+  (ad-set-arg 0 t))
+(ad-activate 'quit-window)
+
 (require 'dabbrev)
 (setq dabbrev-always-check-other-buffers t)
 (setq dabbrev-abbrev-char-regexp "\\sw\\|\\s_")
