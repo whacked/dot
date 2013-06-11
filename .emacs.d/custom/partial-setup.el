79539c94bf5e52fe65fa1da1e7d3478df68af1dc
natto <natto@natto-tp.(none)>
2013-06-11 11:09:58 -0400

    partial setup file

---
+++ .emacs.d/custom/partial-setup.el
@@ -0,0 +1,6 @@
+(load-file "~/dot/.emacs.d/custom/package-management.el")
+(load-file "~/dot/.emacs.d/custom/package-common.el")
+
+(load-file "~/dot/.emacs.d/custom/lispy-stuff.el")
+
+(load-file "~/dot/.emacs.d/custom/usual-environment.el")
