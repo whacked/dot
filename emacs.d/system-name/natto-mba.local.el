006c4bdae0cb9c965a15098db85d67dd4425294c
natto <natto@natto-tpy.none>
2016-02-28 21:08:31 -0500

    merge system-name files into config

--- emacs.d/system-name/natto-mba.local.el
+++
@@ -1,8 +0,0 @@
-(load-file "~/dot/emacs.d/custom/full-setup.el")
-(custom-set-variables
- '(org-file-apps (quote ((auto-mode . emacs)
-                         ("\\.mm\\'" . default)
-                         ("\\.x?html?\\'" . default)
-                         ("\\.xoj\\'" . "xournal %s")
-                         ("\\.pdf\\'" . "open -a Preview %s")))))
-
