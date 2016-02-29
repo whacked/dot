42fd7bc06e70d25355b5f32cd5f2c8eaf9319984
natto <natto@natto-tpy.none>
2016-02-28 21:39:50 -0500

    merge rest of custom/*.el into config.org

--- emacs.d/custom/coding.el
+++
@@ -1,8 +0,0 @@
-(setq my-packages (append my-packages '(
-         ;; coding
-         inf-ruby js2-mode json lua-mode markdown-mode ruby-mode rspec-mode yaml-mode zencoding-mode
-         graphviz-dot-mode
-         python-mode
-         rainbow-mode
-         ;; nxhtml
-         )))
