336eaf71e7a07b1a419e7abf75bae41935ad80ff
natto <natto@natto-tpy.none>
2016-05-29 12:39:26 -0700

    sync up to date lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,9 @@
 {:user {:plugins
-        [[lein-ancient "0.5.5"]
-         [cider/cider-nrepl "0.9.0-snapshot"]]
+        [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+
+         ;; old stuff, commenting to avoid build conflicts
+         ;; [lein-pprint "1.1.1"]
+         ;; [lein-ancient "0.6.2"]
+         ;; [lein-externs "0.1.5"]
         }}
 
