ce6783fbd152e28574c709969755123a875dcba8
natto <natto@natto-tpy.none>
2016-11-19 15:55:08 -0800

    upd to path-join org file load path

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,7 @@
-(org-babel-load-file "~/.emacs.d/config.org")
+(defun path-join (&rest path-seq)
+  (concat (mapconcat 
+	   'file-name-as-directory
+	   (butlast path-seq) "")
+	  (car (last path-seq))))
+(setq EMACS.D-DIR (path-join "~" ".emacs.d"))
+(org-babel-load-file (path-join EMACS.D-DIR "config.org"))
