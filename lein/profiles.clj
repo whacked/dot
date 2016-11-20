0e7eecdbe098b82b402c0334b6c2c97ee1c39545
natto <natto@natto-tpy.none>
2016-11-19 16:15:20 -0800

    upd lein profiles alembic / cider nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,7 @@
-{:user {:plugins
-        [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-
+{:user {:dependencies
+        [[alembic "0.3.2"]]
+        :plugins 
+        [[cider/cider-nrepl "0.14.0"]
          ;; old stuff, commenting to avoid build conflicts
          ;; [lein-pprint "1.1.1"]
          ;; [lein-ancient "0.6.2"]
