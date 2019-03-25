6cb18c81efe5dc0c93a6a45904d961c23eaf8e5d
natto <whacked@users.noreply.github.com>
2019-03-24 17:33:01 -0700

    update profiles.clj according to clj-refactor

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,10 @@
 {:user {:dependencies
-        [[alembic "0.3.2"]]
+        [
+         ]
         :plugins 
-        [[cider/cider-nrepl "0.14.0"]
-         ;; old stuff, commenting to avoid build conflicts
-         ;; [lein-pprint "1.1.1"]
-         ;; [lein-ancient "0.6.2"]
-         ;; [lein-externs "0.1.5"]
+        [
+         [refactor-nrepl "2.4.0"]
+         [cider/cider-nrepl "0.18"]
          ]
         }}
 
