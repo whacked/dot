2ba0bc6c934684bbddfb422b4008114a338da4c6
natto <whacked@users.noreply.github.com>
2020-06-06 11:14:43 -0700

    cleanup emacs shared initialzation logic

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,19 +1,4 @@
-;; package management
-(setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("ELPA"      . "http://tromey.com/elpa/")
-                         ("org"       . "http://orgmode.org/elpa/")
-                         ("melpa"     . "https://melpa.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
-;; activate all the packages (in particular autoloads)
-(package-initialize)
-
-(defun path-join (&rest path-seq)
-  (concat (mapconcat 
-	   'file-name-as-directory
-	   (butlast path-seq) "")
-	  (car (last path-seq))))
-(setq EMACS.D-DIR (path-join "~" ".emacs.d"))
-(org-babel-load-file (path-join EMACS.D-DIR "config.org"))
+(org-babel-load-file "~/dot/emacs.d/config.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
