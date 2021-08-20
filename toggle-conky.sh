2740977144ced33b10a7021078383f3bedbd2a1c
whacked-xps <whacked@users.noreply.github.com>
2021-08-20 10:07:58 +0800

    update toggle-conky to detect active monitor via mouse position

--- toggle-conky.sh
+++ toggle-conky.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+
 MONITOR_NUMBER=0
+MOUSE_X=$(xdotool getmouselocation --shell | grep X= | cut -d= -f2)
+if [ $MOUSE_X -gt 1920 ]; then
+    MONITOR_NUMBER=1
+fi
+
 if pgrep conky; then
     pkill conky
 else
