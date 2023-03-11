bc4985e0696003355fffc14ef751350776e0eb82
whacked-xps <whacked@users.noreply.github.com>
2023-03-11 11:12:50 +0800

    conky: add handler for swaywm

--- toggle-conky.sh
+++ toggle-conky.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-EDP1_MONITOR_NUMBER=$(xrandr --listactivemonitors | grep eDP-1 | cut -d: -f1)
-MOUSE_X=$(xdotool getmouselocation --shell | grep X= | cut -d= -f2)
-if [ $MOUSE_X -gt 1920 ]; then
-    MONITOR_NUMBER=$((1 - $EDP1_MONITOR_NUMBER))
+if [ $DESKTOP_SESSION = "sway" ]; then
+    MONITOR_NUMBER=1
 else
-    MONITOR_NUMBER=$EDP1_MONITOR_NUMBER
+    EDP1_MONITOR_NUMBER=$(xrandr --listactivemonitors | grep eDP-1 | cut -d: -f1)
+    MOUSE_X=$(xdotool getmouselocation --shell | grep X= | cut -d= -f2)
+    if [ $MOUSE_X -gt 1920 ]; then
+        MONITOR_NUMBER=$((1 - $EDP1_MONITOR_NUMBER))
+    else
+        MONITOR_NUMBER=$EDP1_MONITOR_NUMBER
+    fi
 fi
 
 if pgrep conky; then
     pkill conky
 else
+    # override for PATH if env doesn't supply nix-enabled
+    # PATH=$HOME/.nix-profile/bin:$PATH
     conky -m $MONITOR_NUMBER -c $HOME/dot/conkyrc &
 fi
