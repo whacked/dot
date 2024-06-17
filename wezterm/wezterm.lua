715e80f673365e76e8f8e0a5bbd185addcb334ed
whacked-xps <whacked@users.noreply.github.com>
2024-06-17 23:38:52 +0800

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,10 +4,15 @@ local config = {}
 
 config.color_scheme = 'Mirage'
 config.window_background_opacity = 0.8
-config.font = wezterm.font_with_fallback {
-  "FiraCode",
-  "Deja Vu Sans Mono"
-};
+config.hide_tab_bar_if_only_one_tab = true
+-- leave with defaults for now
+-- config.font = wezterm.font_with_fallback {
+--   "FiraCode",
+--   "Deja Vu Sans Mono",
+--   "Menlo",              -- mac fallback
+--   "Consola",            -- windows fallback
+--   "Liberation Mono",    -- linux fallback?
+-- };
 config.font_size = 12.0;
 
 
